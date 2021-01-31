e3a0206a0fe4158166c36d09e5f645a29430a7ef
Michael <42353725+eitherneither@users.noreply.github.com>
2021-01-31 16:07:40 +0300

    310121

--- AverageScore.py
+++ AverageScore.py
@@ -4,59 +4,63 @@ import tabula
 import os
 import re
 
-ntype = []
 
+class pdfTableValues():
 
-def convert(column):    # Convert string to float
-    for translate in column:
-        trans = translate.replace(',', '.')
-        ntype.append(float(trans))
-    b = 0
-    for cnvscores in ntype:
-        b = b+cnvscores
-        roundCScore = round(b/len(ntype))
-        normalCScore = b/len(ntype)
-        os.system('cls')
-        print('Round:   ', roundCScore, '\n', 'Average: ', normalCScore)
-
-
-def average_score():    # Find average score for one lesson
-    pdffile = input('Pdf Location: ')
-    findSpan = re.search('[a-zA-Z0-9]*.pdf$', pdffile)
-    tuple2list = [i for i in findSpan.span()]
-    spoint = tuple2list[0]
-    epoint = tuple2list[1]-4
-    outputName = pdffile[spoint:epoint]+'.csv'
-    if os.path.exists(outputName) == 'True':
-        pass
-    else:
-        tabula.convert_into(pdffile, outputName, pages='all')
-
-    df = pd.read_csv(outputName)
-    z = dict(enumerate(df.columns.values, start=1))     # 1
-    print(z)
-    nmbr = int(input('Enter number: '))
-    column_name = z[nmbr]
-    df1 = df[column_name].values.tolist()   # 2
-    r = re.compile(',')
-    if df[column_name].dtype == 'object' or 'int':  # 3
-        if any(r.search(str(comma)) for comma in df1) is True:  # 4
-            df.fillna(0, inplace=True)
-            convert(df1)
+    def __init__(self):
+        pdffile = input('Pdf Location: ')
+        self.name = os.path.basename(pdffile)
+        self.name = self.name.replace('pdf', 'csv')
+        # findSpan = re.search('[a-zA-Z0-9]*.pdf$', pdffile)
+        # spanLocation = [i for i in findSpan.span()]
+        # spoint = spanLocation[0]
+        # epoint = spanLocation[1]-4
+        # self.outputName = pdffile[spoint:epoint]+'.csv'
+        if os.path.exists(self.name) == 'True':
+            pass
         else:
-            df[column_name] = pd.to_numeric(df[column_name], errors='coerce')  # 5
-            df.fillna(0, inplace=True)  # 6
-            df1 = df[column_name].values.tolist()
-            a = 0
-            for scores in df1:
-                a = a+scores
-            roundScore = round(a/len(df1))
-            normalScore = a/len(df1)
-            os.system('cls')
-            print('Round:   ', roundScore, '\n', 'Average: ', normalScore)
+            tabula.convert_into(pdffile, self.name, pages='all')
+
+    def convert(self, column):
+        ntype = []
+        for translate in column:
+            trans = translate.replace(',', '.')
+            ntype.append(float(trans))
+        b = 0
+        for cnvscores in ntype:
+            b = b+cnvscores
+            roundCScore = round(b/len(ntype))
+            normalCScore = b/len(ntype)
+        os.system('cls')
+        print('Round:   ', roundCScore, '\nAverage: ', normalCScore)
+
+    def averageScore(self):
+        df = pd.read_csv(self.name)
+        z = dict(enumerate(df.columns.values, start=1))  # 1
+        print(z)
+        nmbr = int(input('Enter number: '))
+        columName = z[nmbr]
+        df1 = df[columName].values.tolist()  # 2
+        r = re.compile(',')
+        if df[columName].dtype == 'object' or 'int':  # 3
+            if any(r.search(str(comma)) for comma in df1) is True:  # 4
+                df.fillna(0, inplace=True)
+                self.convert(df1)
+            else:
+                df[columName] = pd.to_numeric(df[columName], errors='coerce')  # 5
+                df.fillna(0, inplace=True)  # 6
+                df1 = df[columName].values.tolist()
+                a = 0
+                for scores in df1:
+                    a = a+scores
+                roundScore = round(a/len(df1))
+                normalScore = a/len(df1)
+                os.system('cls')
+                print('Round:   ', roundScore, '\nAverage: ', normalScore)
 
 
-average_score()
+pdfTableValues().averageScore()
+
 
 # 1 = Get column names
 # 2 = Convert column values to list
